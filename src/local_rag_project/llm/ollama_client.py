a7da945f415e2e5ce15cb36b13f4dacfc0956579
Samir Vaniya <skvaniya6275@gmail.com>
2026-09-02 01:30:24 +0530

    feat: add chat history feature

--- src/local_rag_project/llm/ollama_client.py
+++ src/local_rag_project/llm/ollama_client.py
@@ -2,6 +2,7 @@ from ollama import chat
 
 MODEL_NAME = "llama3.2:3b"
 
+# First version - for testing. No streaming and full response after complete generataion
 def ask_llm(prompt: str) -> str:
     response = chat(
         model=MODEL_NAME,
@@ -15,3 +16,10 @@ def ask_llm(prompt: str) -> str:
 
     return response
 
+def chat_with_llm(messages: list[dict]) -> str:
+    response = chat(
+        model=MODEL_NAME,
+        messages=messages
+    )
+
+    return response.message.content
--- src/local_rag_project/main.py
+++ src/local_rag_project/main.py
@@ -1,21 +1,38 @@
-from local_rag_project.llm.ollama_client import ask_llm
+# from local_rag_project.llm.ollama_client import ask_llm
+from local_rag_project.llm.ollama_client import chat_with_llm
 
 def main():
     print("="*50)
     print("Local RAG Project")
     print("="*50)
 
+    messages = []
+
     while True:
         question = input("\nYOU : ")
 
         if question.lower() in {"exit","quit","q"}:
-            print("Goodbye")
+            print("\nGoodbye")
             break
 
-        answer = ask_llm(question)
+        messages.append(
+            {
+                "role": "user",
+                "content": question,
+            }
+        )
+
+        answer = chat_with_llm(messages=messages)
+
+        messages.append(
+            {
+            "role": "assistant",
+            "content": answer,
+            }
+        )
 
         print("\nAI : ")
-        print(answer.message.content)
+        print(answer)
 
 
 if  __name__=="__main__":
